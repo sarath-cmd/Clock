d7c11a3adca2f024326d3772ae39085f093c3538
sarath-cmd <sarathredmagic@gmail.com>
2024-04-11 21:15:28 +0530

    first commit

--- script/switch.js
+++ script/switch.js
@@ -1,21 +1,21 @@
-function watchandstop()
-{
-    var forswitch = document.getElementById('watch');
-    var tostopwatchbtn = document.getElementById('tostopwatch');
-    var toclockbtn = document.getElementById('toclock');
-    var stopwatch = document.getElementById('stopwatch');
-    if (forswitch.classList.contains('hidden'))
-    {
-        forswitch.classList.remove('hidden');
-        tostopwatchbtn.classList.remove('hidden');
-        toclockbtn.classList.add('hidden');
-        stopwatch.classList.add('hidden')
-    }
-    else
-    {
-        forswitch.classList.add('hidden');
-        tostopwatchbtn.classList.add('hidden');
-        toclockbtn.classList.remove('hidden');
-        stopwatch.classList.remove('hidden');
-    }
-}
+// function watchandstop()
+// {
+//     var forswitch = document.getElementById('watch');
+//     var tostopwatchbtn = document.getElementById('tostopwatch');
+//     var toclockbtn = document.getElementById('toclock');
+//     var stopwatch = document.getElementById('stopwatch');
+//     if (forswitch.classList.contains('hidden'))
+//     {
+//         forswitch.classList.remove('hidden');
+//         tostopwatchbtn.classList.remove('hidden');
+//         toclockbtn.classList.add('hidden');
+//         stopwatch.classList.add('hidden')
+//     }
+//     else
+//     {
+//         forswitch.classList.add('hidden');
+//         tostopwatchbtn.classList.add('hidden');
+//         toclockbtn.classList.remove('hidden');
+//         stopwatch.classList.remove('hidden');
+//     }
+// }
